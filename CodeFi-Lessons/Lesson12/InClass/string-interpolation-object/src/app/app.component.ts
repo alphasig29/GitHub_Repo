183c5f54b570ea059a41d840d524429904c99259
alphasig29 <99698498+alphasig29@users.noreply.github.com>
2022-03-24 20:45:52 -0500

    Class 12 Completed Exercise #4 of 7

--- CodeFi-Lessons/Lesson12/InClass/string-interpolation-object/src/app/app.component.ts
+++ CodeFi-Lessons/Lesson12/InClass/string-interpolation-object/src/app/app.component.ts
@@ -6,6 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'string-interpolation-exercise';
-  name: string = 'William van Wilder';
+  title = 'string-interpolation-object';
+  heading: string = 'user post';
+  num: number = 1;
+
+  
 }
