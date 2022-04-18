aeccafb9781ef7b5abba138330e33349eabda082
alphasig29 <99698498+alphasig29@users.noreply.github.com>
2022-04-17 19:52:47 -0500

    Lesson 19 -partially competed Forms Part 2 (Reactive Forms)

--- CodeFi-Lessons/Lesson19/PreWork/forms-reactive-start/src/app/app.component.ts
+++ CodeFi-Lessons/Lesson19/PreWork/forms-reactive-start/src/app/app.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   genders = ['male', 'female'];
+  signupForm: FormGroup;
+  forbiddenUsernames = ['Chris', 'Anna'];
+
+ngOnInit(): void {
+    this.signupForm = new FormGroup({
+      'userData': new FormGroup({
+        'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
+        'email': new FormControl(null, [Validators.required, Validators.email]),
+      }),
+      'gender': new FormControl('male', Validators.required),
+      'hobbies': new FormArray([])
+    });
+}
+
+onSubmit(){
+  console.log(this.signupForm);
+}
+
+getControls() {
+  return (<FormArray>this.signupForm.get('hobbies')).controls;
+}
+onAddHobbies(){
+  const control = new FormControl(null, Validators.required);
+  //must cast as formArray
+  (<FormArray>this.signupForm.get('hobbies')).push(control);
+}
+
+// in the Validator array, must bind 'this'
+// to get this.forbiddenuernames in scope in funciton below
+forbiddenNames(control: FormControl): {[s: string]: boolean} {
+  if (this.forbiddenUsernames.indexOf(control.value) !== -1) {
+    return {'nameIsForbidden': true};
+  }
+  // must return nothing or null, should not pass false
+  return null;
+}
+
 }
